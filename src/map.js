671777f29aaa982193e2c4abc16a5849c747af6e
Paul Mairot <paul.mairot@heig-vd.ch>
2022-04-01 23:24:16 +0200

    Changing year on scroll

--- src/map.js
+++ src/map.js
@@ -1,1 +1,17 @@
-console.log("testMap");
+import * as d3 from 'd3'
+
+
+let year = 1500;
+let textYear = d3.select('#year');
+let lastScrollTop = 0;
+
+window.addEventListener('wheel', (e) => {  
+    if (e.deltaY < 0) {
+        if (year > 1500)    
+            year--
+    } else {
+        if (year < 2022)    
+            year++
+    }
+    textYear.text(year);
+})
