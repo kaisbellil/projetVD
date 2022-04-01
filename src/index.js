31d52fad0f45faef0e6a46304abf3820e2fa2d5b
Paul Mairot <paul.mairot@heig-vd.ch>
2022-04-01 14:00:18 +0200

    Create files (html + js)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as d3 from 'd3'
 
+console.log("testIndex");
+
 // Pour importer les données
 // import file from '../data/data.csv'
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
-const path = require('path');const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+/*
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './public/index.html',
     filename: 'index.html',
     inject: 'body'
 })
+*/
 
 module.exports = {
     name: 'browser',
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        map: './src/map.js'
+    },
     output: {
         path: path.resolve('dist'),
-        filename: 'index_bundle.js'
+        filename: '[name].bundle.js'
     },
     module: {
         rules: [
@@ -25,5 +31,22 @@ module.exports = {
             }
         ]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            chunks: 'index',
+            excludeChunks: ['map'],
+            filename: 'index.html',
+            inject: 'body'
+            
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/map/index.html',
+            chunks: 'map',
+            excludeChunks: ['index'],
+            filename: 'map/index.html',
+            inject: 'body'
+            
+        })
+    ]
 }
