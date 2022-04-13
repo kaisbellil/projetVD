d47e9d5d27653462359a326ec9ac1221bccd6796
Paul Mairot <paul.mairot@heig-vd.ch>
2022-04-13 08:53:17 +0200

    Adding function GetConflictsByYear()
    Function to get all conflicts present and past based on a year passed in param.

--- src/map.js
+++ src/map.js
@@ -1,6 +1,33 @@
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+import conflicts from '../data/conflicts.csv';
+
+function getConflictsByYear(year) {
+    let conflictsList = [];
+
+    conflicts.forEach(conflic => {
+        if (conflic.Date <= year && conflic.Date >= 1500) {
+            conflictsList.push(conflic);
+        }
+    });
+
+    return conflictsList;
+}
+
+//console.log(conflicts);
+
+let margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 650 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+let svg = d3.select("#map")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+// Changing year with mouse wheel (scroll)
 let year = 1500;
 let textYear = d3.select('#year');
 let lastScrollTop = 0;
@@ -14,4 +41,11 @@ window.addEventListener('wheel', (e) => {
             year++
     }
     textYear.text(year);
+    
+    console.log(getConflictsByYear(year));
+
 })
+
+let list = getConflictsByYear(1900);
+
+//console.log(list);
